3eba3c73c5f2d119d7c17e8c0d83fca9bfc7e149
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2024-06-24 22:12:37 +0530

    Chore: Sending some more data

--- packages/server/lib/modes/run.ts
+++ packages/server/lib/modes/run.ts
@@ -707,8 +707,8 @@ async function waitForTestsToFinishRunning (options: { project: Project, screens
   }
 
   if (captured) {
-    debug(`_after:spec:stdout called: ${captured.toString()}`)
-    await runEvents.execute('_after:spec:stdout', { spec: publicSpec, stdout: captured.toString() })
+    debug(`_after:spec:stdout called`)
+    await runEvents.execute('_after:spec:stdout', { publicSpec, stdout: captured.toString(), spec, results })
   }
 
   // the early exit terminator persists between specs,
