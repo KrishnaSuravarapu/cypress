77bd3982c64a30ffa1b367b8b5705cdbd34b7662
Krishna Suravarapu <36037520+KrishnaSuravarapu@users.noreply.github.com>
2024-06-24 15:20:58 +0530

    Chore: Some sending relative name in specname

--- packages/server/lib/modes/run.ts
+++ packages/server/lib/modes/run.ts
@@ -448,7 +448,7 @@ async function waitForBrowserToConnect (options: { project: Project, socketId: s
   // path for next spec in launch browser.
   // we need it to run on every spec even in single browser mode
   currentSetScreenshotMetadata = (data) => {
-    data.specName = spec.relativeToCommonRoot
+    data.specName = spec.relative
 
     return data
   }
